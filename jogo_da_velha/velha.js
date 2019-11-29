3102d70264fac2dc943c22339045f4e6291a5cdf
Thiago Callegari <calegari_8@hotmail.com>
2019-11-28 23:08:32 -0300

    Jogo da Leia

--- jogo_da_velha/velha.js
+++ jogo_da_velha/velha.js
@@ -7,11 +7,11 @@ function joga(idDiv){
     var textoInterno = document.getElementById(idDiv).innerHTML;
     if (textoInterno == ""){
         if (vezDoJogador == 1){
-            document.getElementById(idDiv).innerHTML="<img src='JogodaVelha01.jpg' height: 100px;>";
+            document.getElementById(idDiv).innerHTML="<img src='leia.png' class='img-fluid image w-auto p-4' alt='Imagem responsiva';>";
             vezDoJogador = 2;
         }
         else{
-            document.getElementById(idDiv).innerHTML="<img src='JogodaVelha02.jpg' height: 100px;>";
+            document.getElementById(idDiv).innerHTML="<img src='darth.png' class='img-fluid image w-auto p-4' alt='Imagem responsiva';>";
             vezDoJogador = 1;
         }
     }
